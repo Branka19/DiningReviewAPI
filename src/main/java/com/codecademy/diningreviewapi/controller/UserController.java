d5804c15f8b2e7aa8f2fa94bf1cb8c51adf9a280
Judy <brankanails@gmail.com>
2024-08-17 11:16:29 +0200

    User controller updated with login authentication.

--- src/main/java/com/codecademy/diningreviewapi/controller/UserController.java
+++ src/main/java/com/codecademy/diningreviewapi/controller/UserController.java
@@ -3,6 +3,13 @@ package com.codecademy.diningreviewapi.controller;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.codecademy.diningreviewapi.enums.ReviewStatus;
+import com.codecademy.diningreviewapi.exceptions.RestaurantUserNotFoundException;
 import com.codecademy.diningreviewapi.model.DiningReview;
 import com.codecademy.diningreviewapi.model.Restaurant;
 import com.codecademy.diningreviewapi.model.RestaurantUser;
@@ -23,67 +31,78 @@ import com.codecademy.diningreviewapi.repository.UserRepo;
 
 @RequestMapping("/users")
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
-	
+
 	private final UserRepo userRepo;
 	private final RestaurantRepo restaurantRepo;
 	private final DiningReviewRepo diningReviewRepo;
-	
+	private final PasswordEncoder passwordEncoder;
+	private final AuthenticationProvider authprovider;
+
 	public UserController(final UserRepo userRepo, final RestaurantRepo restaurantRepo,
-			final DiningReviewRepo diningReviewRepo) {
+			final DiningReviewRepo diningReviewRepo, final PasswordEncoder encoder, final AuthenticationProvider authprovider) {
 		this.userRepo = userRepo;
 		this.restaurantRepo = restaurantRepo;
 		this.diningReviewRepo = diningReviewRepo;
+		this.passwordEncoder = encoder;
+		this.authprovider = authprovider;
 	}
 	
-	//fetch the user profile belonging to a given display name
-	@GetMapping("/{displayName}")
-	public RestaurantUser getUserByDisplayName(@PathVariable String displayName)
+	@PostMapping("/login")
+	public ResponseEntity<String> login(@RequestBody RestaurantUser userLogin)
 	{
+		Authentication authenticationRequest  = 
+				UsernamePasswordAuthenticationToken
+				.unauthenticated(userLogin.getDisplayName(), userLogin.getPassword());
+		this.authprovider.authenticate(authenticationRequest);
 		
-		Optional<RestaurantUser> userToFindOptional = this.userRepo.findByDisplayName(displayName);
-		 
-		 if(!userToFindOptional.isPresent()) 
-		 { 
-			 throw new ResponseStatusException(HttpStatus.NOT_FOUND, "A user with the given user name is not found.");
-		 }
-		 
-		 RestaurantUser user = userToFindOptional.get();
-		 return user;
-	 
+		return new ResponseEntity<String>("Successful Login", HttpStatus.ACCEPTED);
 	}
 	
-	//As an unregistered user, create a user profile using a display name that’s unique
+
+	// fetch the user profile belonging to a given display name
+	@GetMapping("/{displayName}")
+	public RestaurantUser getUserByDisplayName(@PathVariable String displayName) {
+
+		Optional<RestaurantUser> userToFindOptional = this.userRepo.findByDisplayName(displayName);
+
+		if (!userToFindOptional.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "A user with the given user name is not found.");
+		}
+
+		RestaurantUser user = userToFindOptional.get();
+		return user;
+
+	}
+
+	// As an unregistered user, create a user profile using a display name that’s unique
 	@PostMapping("/addNew")
 	public RestaurantUser createNewUser(@RequestBody RestaurantUser user) {
-		
-		if (userRepo.findByDisplayName(user.getDisplayName()).isPresent())
-		{
+
+		if (userRepo.findByDisplayName(user.getDisplayName()).isPresent()) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A user with this username already exists");
 		}
+		//encodes the password before saving it
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		RestaurantUser newUser = this.userRepo.save(user);
 
 		return newUser;
 	}
-	
-	//As a registered user, I want to update my user profile. I cannot modify my unique display name.
+
+	// As a registered user, I want to update my user profile. I cannot modify my unique display name.
 	@PutMapping("/{displayName}")
-	public RestaurantUser updateUser(
-			@PathVariable String displayName,
-			@RequestBody RestaurantUser user) {
+	public RestaurantUser updateUser(@PathVariable String displayName, @RequestBody RestaurantUser user) {
 		Optional<RestaurantUser> userToUpdateOptional = this.userRepo.findByDisplayName(displayName);
-		if (!userToUpdateOptional.isPresent()) 
-		{
+		if (!userToUpdateOptional.isPresent()) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "A user with this name is not found.");
 		}
-		
+
 		RestaurantUser userToUpdate = userToUpdateOptional.get();
 
-		if(user.getDisplayName() != null)
-		{
-			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You cannot change your display name.");
+		if (user.getPassword() != null)	{
+			userToUpdate.setPassword(userToUpdate.getPassword());
 		}
-		
 		if (user.getCity() != null) {
 			userToUpdate.setCity(user.getCity());
 		}
@@ -106,80 +125,68 @@ public class UserController {
 		return updatedUser;
 	}
 
-	//registered user creates a new review
-	@PostMapping("/reviews/addNew")
-	public DiningReview createNewReview(
-			@RequestParam String userName,
-			@RequestParam String restaurantName,
-			@RequestParam String zipCode,
-			@RequestParam(required = false) String peanutScore,
-			@RequestParam(required = false) String eggScore,
-			@RequestParam(required = false) String dairyScore,
+	// registered user creates a new review
+	@PostMapping("/{userName}/reviews/addNew")
+	public DiningReview createNewReview(@RequestParam String userName, @RequestParam String restaurantName,
+			@RequestParam String zipCode, @RequestParam(required = false) String peanutScore,
+			@RequestParam(required = false) String eggScore, @RequestParam(required = false) String dairyScore,
 			@RequestParam(required = false) String comment) {
 
-		if (!userRepo.findByDisplayName(userName).isPresent())
-		{
+		if (!userRepo.findByDisplayName(userName).isPresent()) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "You have to register to submit a review!");
 		}
 
-		Optional<Restaurant> restaurantToFindOptional = restaurantRepo.findByRestaurantNameAndRestaurantZipCode(restaurantName, zipCode);
-		
-		if(!restaurantToFindOptional.isPresent())
-			{
+		Optional<Restaurant> restaurantToFindOptional = restaurantRepo
+				.findByRestaurantNameAndRestaurantZipCode(restaurantName, zipCode);
+
+		if (!restaurantToFindOptional.isPresent()) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given restaurant is not found.");
-			}
-		
+		}
+
 		Integer peanuts = getScore(peanutScore);
 		Integer eggs = getScore(eggScore);
 		Integer dairy = getScore(dairyScore);
 
 		Integer restaurantId = restaurantToFindOptional.get().getId();
-		
-		DiningReview newReview = new DiningReview(userName, restaurantId, peanuts, eggs, dairy, comment, ReviewStatus.PENDING);
+
+		DiningReview newReview = new DiningReview(userName, restaurantId, peanuts, eggs, dairy, comment,
+				ReviewStatus.PENDING);
 		return this.diningReviewRepo.save(newReview);
 	}
-		
-		private Integer getScore(String scoreString)
-		{
-			Integer score = null;
-			
-			if(!scoreString.isEmpty()){
-				boolean isNumeric = scoreString.chars().allMatch( Character::isDigit );
-				
-				if(!isNumeric)
-				{
-					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You entered an invalid score.");
-				}
-				
-				score = Integer.parseInt(scoreString);
-				
-				if(score < 1 || score > 5)
-				{
-					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Your score should be between 1-5.");
-				}
+
+	private Integer getScore(String scoreString) {
+		Integer score = null;
+
+		if (!scoreString.isEmpty()) {
+			boolean isNumeric = scoreString.chars().allMatch(Character::isDigit);
+
+			if (!isNumeric) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You entered an invalid score.");
 			}
-			
-			return score;
+
+			score = Integer.parseInt(scoreString);
+
+			if (score < 1 || score > 5) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Your score should be between 1-5.");
+			}
 		}
+
+		return score;
+	}
 
 	// user updates an existing review
 	@PutMapping("/{userName}/reviews/{reviewId}")
-	public DiningReview updateReview(
-			@PathVariable String userName,
-			@PathVariable Integer reviewId,
-			@RequestBody DiningReview newReview)
-		{
-		
+	public DiningReview updateReview(@PathVariable String userName, @PathVariable Integer reviewId,
+			@RequestBody DiningReview newReview) {
+
 		Optional<DiningReview> reviewToUpdateOptional = this.diningReviewRepo.findByUserNameAndId(userName, reviewId);
-		if (!reviewToUpdateOptional.isPresent())
-		{
+		if (!reviewToUpdateOptional.isPresent()) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "A review with this id is not found.");
 		}
-		
-		//old version of the review
+
+		// old version of the review
 		DiningReview reviewToUpdate = reviewToUpdateOptional.get();
-		
-		
+
 		if (newReview.getCommentary() != null) {
 			reviewToUpdate.setCommentary(newReview.getCommentary());
 		}
@@ -193,11 +200,21 @@ public class UserController {
 		if (newReview.getPeanutScore() != null) {
 			reviewToUpdate.setPeanutScore(newReview.getPeanutScore());
 		}
-		
+
 		reviewToUpdate.setReviewStatus(ReviewStatus.PENDING);
 
 		DiningReview updatedReview = this.diningReviewRepo.save(reviewToUpdate);
 		return updatedReview;
 	}
-	
+
+	// delete a user
+	@DeleteMapping("/{id}")
+	String deleteUser(@PathVariable Integer id) {
+		if (!userRepo.existsById(id))
+			throw new RestaurantUserNotFoundException(id);
+
+		userRepo.deleteById(id);
+		return "User with id " + id + " has been deleted.";
+	}
+
 }
